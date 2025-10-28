a41c6b75eb8eac590c61a6a310166b2e684af67c
BogdanSuh <sbv.220613@gmail.com>
2025-10-28 15:58:18 +0700

    6 часть

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -36,6 +36,14 @@ class Book(models.Model):
         """
         return self.title
 
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
+
 
     def get_absolute_url(self):
         """
@@ -96,3 +104,4 @@ class Author(models.Model):
                 String for representing the Model object.
                 """
                 return '%s, %s' % (self.last_name, self.first_name)
+
